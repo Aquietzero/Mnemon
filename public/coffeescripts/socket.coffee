e82b48024e96731cda3f779bb49e45e67ce5a9d5
Aquietzero <zhaoyunhaosss@gmail.com>
2017-07-26 15:50:25 +0800

    mod: connect socket in a better way.

--- public/coffeescripts/socket.coffee
+++ public/coffeescripts/socket.coffee
@@ -1,3 +1,4 @@
-socket = io 'http://120.55.125.152:8888'
+# socket = io 'http://120.55.125.152:8888'
+socket = io '/', port: 8888
 
 module.exports = socket
--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -19538,7 +19538,9 @@
 
 	var socket;
 
-	socket = io('http://120.55.125.152:8888');
+	socket = io('/', {
+	  port: 8888
+	});
 
 	module.exports = socket;
 
